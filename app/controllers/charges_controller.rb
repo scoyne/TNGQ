ea7163523acfb3973d325c213d9e3087da49c954
scoyne <scoyne@bw.edu>
2018-06-03 12:57:31 -0400

    commit for troublshooting

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,25 +1,54 @@
 class ChargesController < ApplicationController
-    def new
+  after_action :premium_role
+
+  def new
+      @stripe_btn_data = {
+        key: "#{ Rails.configuration.stripe[:publishable_key] }",
+        description: "Premium Membership - #{current_user.email}",
+        amount: @amount
+      }
     end
 
     def create
-    # Amount in cents
-    @amount = 500
-  
-    customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
-    )
-  
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
-    )
-  
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
-  end
+      # Amount in cents
+      @amount = 1500
+    
+      customer = Stripe::Customer.create(
+        :email => params[:stripeEmail],
+        :source  => params[:stripeToken]
+      )
+    
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @amount,
+        :description => 'Premium Membership',
+        :currency    => 'usd'
+      )
+      
+      flash[:success] = "Thank you for your membership! #{current_user.email}!"
+      redirect_to charges_path
+
+      rescue Stripe::CardError => e
+        flash[:error] = e.message
+        redirect_to new_charge_path
+    end
+
+
+    def downgrade
+        current_user.up_attribute(:role, 'standard')
+
+        if current_user.save
+          flash[:notice] = "You have successfully cancelled your Premium Membership."
+          redirect_to root_path
+        else
+          flash[:alert] = "There was a problem cancelling your account.  Please try again."
+          redirect_to root_path
+        end
+    end
+
+    private
+    def premium_role
+        current_user.update_attribute(:role, 'premium')
+        current_user.save
+    end
 end
