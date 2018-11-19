590d9c6ac07c45134b9fee1c3807ef921331c419
scoyne <scoyne@bw.edu>
2018-11-18 20:45:57 -0500

    updated producton.rb adding asset compiler to displace css images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,11 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+
+  # Use an asset compiler
+  config.serv_static_assets = true
+  config.assets.compile = true
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
